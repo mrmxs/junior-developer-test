a83a0a8afd2b2e3661a41b2087d3cd1f353dce5e
mrmxs <maryzmaxs@gmail.com>
2018-02-12 01:15:21 +0300

    done ReverseTest

---
+++ ReverseTest/ReverseApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReverseApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            var bytes = new byte[10];
+
+            random.NextBytes(bytes);
+            Reverse(bytes);
+        }
+
+        public static void Reverse(byte[] bytes)
+        {
+            Console.WriteLine("Test bytes___________    Reversed_____________");
+
+            foreach (var b in bytes)
+            {
+                var int32 = (int) b;
+                var result = 0;
+
+                for (var i = 0; i < 8; i++, int32 >>= 1, result <<= 1)
+                {
+                    result |= int32 & 1;
+                }
+                result >>= 1;
+
+                Console.WriteLine("{0, 10} {1, 10} -> {2, 10} {3, 10}",
+                    b, Convert.ToString(b, 2), result, Convert.ToString(result, 2)
+                );
+            }
+        }
+    }
+}
